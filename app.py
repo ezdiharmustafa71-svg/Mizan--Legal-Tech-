c1b5efbc64077d965bff0f64badb1ae6a86f317a
ezdiharmustafa71-svg <ezdiharmustafa71@gmail.com>
2026-04-26 15:19:33 +0400

    Fix analyze button functionality to send request

--- app.py
+++ app.py
@@ -7,4 +7,12 @@ user_input = st.text_area("Paste contract text here:")
 
 if st.button("Analyze"):
     if user_input:
-        st.write("Analyzing...")
+    response = requests.post(
+    "http://127.0.0.1:8000/analyze",
+    params={"text": user_input}
+)
+
+result = response.json()
+
+st.write("### Result")
+st.write(result)
